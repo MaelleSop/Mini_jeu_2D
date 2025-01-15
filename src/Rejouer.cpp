d69fc6d4009950a3fcce941e427a4df71eeb3fdd
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 14:24:32 -0500

    Update

--- src/Rejouer.cpp
+++ src/Rejouer.cpp
@@ -1,29 +1,54 @@
 #include "Rejouer.h"
+#include "MainWindow.h"
+#include "MyScene.h"
 
-Rejouer::Rejouer(QObject* parent) : QGraphicsScene(parent){
-    this->setSceneRect(0, 0, 500, 800);
 
-    QVBoxLayout* layoutV = new QVBoxLayout();
+Rejouer::Rejouer(QString newPseudo, QWidget* parent) : QMainWindow(parent), pseudo(newPseudo){
+    this->resize(500,840);
 
-    QGraphicsTextItem* perdu = new QGraphicsTextItem("VOUS AVEZ PERDU !");
-    this->addItem(perdu);
+    QWidget* widget = new QWidget;
 
-    this->rejouer = new QPushButton(QString("Rejouer"));
-    QPixmap pixmap("../img/rejouer.png"); // Chemin vers l'image
+    QLabel* background = new QLabel(this);
+    background->setPixmap(QPixmap("img/fond_jungle"));
+    background->setGeometry(QRect(0, 0, this->width(), this->height()));
+    background->setAttribute(Qt::WA_TransparentForMouseEvents);
+
+    QVBoxLayout* layout = new QVBoxLayout();
+    widget->setLayout(layout);
+
+    //QLabel message
+    this->txt = new QLabel("Vous avez perdu !");
+    QFont font("Arial", 32);
+    this->txt->setFont(font);
+    this->txt->setStyleSheet("font-weight: bold;");
+    layout->addWidget(txt);
+
+    //QLabel qui affiche le score
+    int nbrBananes = MyScene::nbrBananesRecup;
+    this->score = new QLabel();
+    QString txtScore = "Score : " + QString::number(nbrBananes);
+    this->score->setText(txtScore);
+    QFont font2("Arial", 32);
+    this->score->setFont(font2);
+    this->score->setStyleSheet("font-weight: bold;");
+    layout->addWidget(score);
+
+    //Bouton rejouer
+    this->rejouer = new QPushButton();
+    QPixmap pixmap("img/rejouer.png"); // Chemin vers l'image
     QIcon icon(pixmap);
-    rejouer->setIcon(icon);
-    rejouer->setIconSize(pixmap.size());
+    this->rejouer->setIcon(icon);
+    this->rejouer->setIconSize(pixmap.size());
     connect(rejouer, SIGNAL(clicked()), this, SLOT(relancer()));
-    layoutV->addWidget(rejouer);
+    layout->addWidget(rejouer);
 
+    layout->setAlignment(Qt::AlignCenter);
+    widget->setLayout(layout);
+    this->setCentralWidget(widget);
 }
 
 void Rejouer::relancer(){
-
-}
-
-void Rejouer::drawBackground(QPainter* painter, const QRectF &rect) {
-    Q_UNUSED(rect);
-    QPixmap pixBackground("../img/jungle2.jpg");
-    painter->drawPixmap(QPointF(0,0), pixBackground, sceneRect());
+    close();
+    MainWindow* mainWindow = new MainWindow(pseudo, this);
+    mainWindow->show();
 }
