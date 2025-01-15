4185d0d9bc63d4903e1185d5656e1d620766f592
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 14:24:50 -0500

    Update

--- src/Rejouer.h
+++ src/Rejouer.h
@@ -1,25 +1,36 @@
 #ifndef MINIPROJET_REJOUER_H
 #define MINIPROJET_REJOUER_H
 
+#include <iostream>
+#include <QtWidgets>
+#include <QGraphicsProxyWidget>
 #include <QGraphicsScene>
 #include <QGraphicsRectItem>
 #include <QGraphicsPixmapItem>
 #include <QTimer>
 #include <QPainter>
 #include <QPushButton>
+#include <QTextEdit>
 #include <QVBoxLayout>
-
+#include <QLabel>
+#include <QGraphicsView>
+#include <QMainWindow>
+#include <QWidget>
+#include <QScreen>
 
 using namespace std;
 
-class Rejouer : public QGraphicsScene {
+class Rejouer : public QMainWindow{
     Q_OBJECT
 private :
+    QLabel* txt;
+    QPushButton* rejouer;
+    QString pseudo;
+    QLabel* score;
 
 public :
-    Rejouer(QObject* parent = nullptr);
-    QPushButton* rejouer;
-    void drawBackground(QPainter* painter, const QRectF &rect);
+    Rejouer(QString newPseudo = "pseudo", QWidget* parent = nullptr);
+
 public slots:
     void relancer();
 };
