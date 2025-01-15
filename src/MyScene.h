5cad9f30eb9f27cda6394ba095735ffe8973fc18
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 14:23:50 -0500

    Update

--- src/MyScene.h
+++ src/MyScene.h
@@ -8,6 +8,14 @@
 #include <QKeyEvent>
 #include <QPainter>
 #include <QLabel>
+#include <QTextEdit>
+#include <QAudioFormat>
+#include <QAudioSink>
+#include <QFile>
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
 #include <string>
 
 using namespace std;
@@ -15,25 +23,31 @@ using namespace std;
 class MyScene : public QGraphicsScene {
     Q_OBJECT
 private :
+    QString pseudo;
+    double vitesse;
     QGraphicsPixmapItem* singe;
+    QGraphicsTextItem* textScore;
+    QGraphicsTextItem* textMeilleurScore;
     QTimer* timer1;
     QTimer* timer2;
     QTimer* timer3;
     QList<QGraphicsPixmapItem*> bananesList;
-    QList<QGraphicsPixmapItem*> buissonsList;
+    QList<QGraphicsPixmapItem*> gorillesList;
 
 public:
-    int nbrBananesRecup;
+    static int nbrBananesRecup;
     static bool gameIsOn;
-    MyScene(QObject* parent = nullptr);
+    MyScene(QString newPseudo = "pseudo", QObject* parent = nullptr);
     virtual ~MyScene();
     void keyPressEvent(QKeyEvent* event);
     void drawBackground(QPainter* painter, const QRectF &rect);
+    void bestScore();
+    void sonBananeManger();
 
 public slots :
-    void update();
+    void updateGame();
     void insererBananes();
-    void insererBuissons();
+    void insererGorilles();
 };
 
 
