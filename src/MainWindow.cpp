beda78b968d1fe87f9ba168b19d078b3305c08ae
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 14:22:24 -0500

    Update

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,60 +1,43 @@
 #include "MainWindow.h"
+#include "Utils.h"
 
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent){
+MainWindow::MainWindow(QString newPseudo, QWidget *parent) : QMainWindow(parent), pseudo(newPseudo){
 
-    this->mainScene = new MyScene;
-
-    //this->lancementScene = new Lancement;
-
+    this->mainScene = new MyScene(newPseudo);
     this->mainView = new QGraphicsView;
     this->mainView->setScene(mainScene);
-    //this->mainView->setScene(lancementScene);
-
 
     this->setCentralWidget(mainView);
     this->setWindowTitle("My main window");
-    this->setFixedSize(500, 800);
+    this->setFixedSize(500, 840);
 
     timer1 = new QTimer(this);
     connect(timer1, SIGNAL(timeout()), this, SLOT(update()));
-    timer1->start(30);
+    timer1->start(10);
 
     regleMenu = menuBar()->addMenu(tr("&Regle"));
     QAction* actionRegle = new QAction(tr("&About"), this);
     connect(actionRegle, SIGNAL(triggered()), this, SLOT(slot_aboutRegle()));
     regleMenu->addAction(actionRegle);
-
-    helpMenu = menuBar()->addMenu(tr("&Help"));
-    QAction* actionHelp = new QAction(tr("&About"), this);
-    connect(actionHelp, SIGNAL(triggered()), this, SLOT(slot_aboutMenu()));
-    helpMenu->addAction(actionHelp);
-
 }
 
 MainWindow::~MainWindow(){
-
+    delete mainScene;
 }
 
 void MainWindow::update(){
-    bool value = MyScene::gameIsOn;
-
-    if(!value){
-        mainScene->clear();
-        this->lastScene = new Rejouer;
-        this->mainView->setScene(lastScene);
+    bool valuePerdu = MyScene::gameIsOn;
+    if(!valuePerdu){
+        close();
+        MyScene::gameIsOn = true;
+        Rejouer* windowRejouer = new Rejouer(pseudo);
+        windowRejouer->show();
     }
-}
-
-void MainWindow::slot_aboutMenu(){
-    QMessageBox msgBox;
-    msgBox.setText("A small QT/C++ projet...");
-    msgBox.setModal(true); // on souhaite que la fenetre soit modale i.e qu'on ne puisse plus cliquer ailleurs
-    msgBox.exec();
 }
 
 void MainWindow::slot_aboutRegle(){
     QMessageBox regles;
-    regles.setText("Voici les règles du jeu :");
+    regles.setText("Voici les règles du jeu :\nL'objectif du petit singe est de mangé le plus de bananes possible en esquivant le méchant gorille. S'il vous attrape vous avez perdu. Attention à vous, plus vous mangez de bananes, plus la vitesse augmente. ");
     regles.setModal(true);
     regles.exec();
 }
