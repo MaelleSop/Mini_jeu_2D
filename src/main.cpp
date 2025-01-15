b35bea6b226fb0e788016e1ec313adfb7c363aad
MaelleSop <108056541+MaelleSop@users.noreply.github.com>
2025-01-15 14:25:37 -0500

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
 #include "MainWindow.h"
+#include "Lancement.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
-    MainWindow mainWindow;
-    mainWindow.show();
+    Lancement pageLancement;
+    pageLancement.show();
     return application.exec();
 }
